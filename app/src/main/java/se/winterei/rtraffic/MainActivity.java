b2aaaf86912853a83c97d7e7e8401be65338a958
Paul S <contact+github@winterei.se>
2017-01-21 22:24:55 +0900

    Center the hero of the play onto the MainActivity, remove redundant references.

--- app/src/main/java/se/winterei/rtraffic/MainActivity.java
+++ app/src/main/java/se/winterei/rtraffic/MainActivity.java
@@ -10,9 +10,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements
+        OnMapReadyCallback
 {
-    public final static String EXTRA_MESSAGE = "se.winterei.rtraffic.MESSAGE";
+    private GoogleMap mMap;
 
     private void setupToolbar ()
     {
@@ -31,18 +41,11 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         setupToolbar();
-    }
 
-    public void sendMessage (View view)
-    {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-
-        //Intent mIntent = new Intent(this, MapsActivity.class);
-        //startActivity(mIntent);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -72,5 +75,26 @@ public class MainActivity extends AppCompatActivity
                 // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
     }
 }
