95d54078d7eba5f2470a84c9efb603b25dc6b0fc
Paul S <contact+github@winterei.se>
2017-01-22 13:46:35 +0900

    Add first working version of the navigation drawer.

--- app/src/main/java/se/winterei/rtraffic/MainActivity.java
+++ app/src/main/java/se/winterei/rtraffic/MainActivity.java
@@ -1,5 +1,7 @@
 package se.winterei.rtraffic;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements
         OnMapReadyCallback
 {
     private GoogleMap mMap;
+    DrawerLayout drawerLayout;
 
     private void setupToolbar ()
     {
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity implements
         setSupportActionBar(myToolbar);
 
         final ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
         ab.setDisplayHomeAsUpEnabled(true);
     }
 
@@ -53,12 +56,18 @@ public class MainActivity extends AppCompatActivity implements
         );
     }
 
+    private void setupNavigationView ()
+    {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupNavigationView();
         setupToolbar();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -89,6 +98,13 @@ public class MainActivity extends AppCompatActivity implements
             case R.id.action_refresh:
                 // User chose the "Favorite" action, mark the current item
                 // as a favorite...
+                return true;
+
+            case android.R.id.home:
+                if (drawerLayout != null)
+                {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
                 return true;
 
             default:
