1c5159b08717bc4fc7282b7c285700110d4dc9ca
Paul S <contact+github@winterei.se>
2017-01-22 23:00:45 +0900

    Fix fab, add map markers.

--- app/src/main/java/se/winterei/rtraffic/MainActivity.java
+++ app/src/main/java/se/winterei/rtraffic/MainActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -144,10 +145,38 @@ public class MainActivity extends AppCompatActivity implements
     {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Ghetto, shameful marker implementation to get by for now.
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.794403, 90.401070)).title("Airport Road (Dhaka-Mymensingh Hwy) and Kemal Ataturk Avenue"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.850420, 90.408418)).title("Airport Road Roundabout"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.746015, 90.394651)).title("Bangla Motor Mor"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.743643, 90.382264)).title("Dhanmondi 6"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.780351, 90.416731)).title("Gulshan Circle 1"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.794847, 90.414213)).title("Gulshan Circle 2").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_traffic_black_18dp)));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.873833, 90.400593)).title("Uttara Housebuilding"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.775280, 90.389939)).title("Jahangir Gate"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.737625, 90.405229)).title("Kakrail Circle"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.737570, 90.409018)).title("Kakrail Road"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.738111, 90.395851)).title("Kazi Nazrul Islam Avenue & Shahbagh"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.764426, 90.389003)).title("Kazi Nazrul Islam Avenue & Bijoy Sharani"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.758477, 90.389871)).title("Kazi Nazrul Islam Avenue & Indira Road"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.758442, 90.383746)).title("Khamar Bari Gol Chottor"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.828733, 90.420070)).title("Khilkhet"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.744144, 90.414286)).title("Malibag Mor"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.750099, 90.413043)).title("Malibag Rail Gate"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.760161, 90.372976)).title("Mirpur Road & Asad Avenue"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.738768, 90.383448)).title("Mirpur Road & Elephant Road"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.758307, 90.374220)).title("Mirpur Road & Manik Mia Avenue"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.756349, 90.375102)).title("Mirpur Road & Old Dhanmondi 27/New 16\n"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.778311, 90.397932)).title("Mohakhali Chourasta"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.745760, 90.412240)).title("Mouchak"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.751346, 90.378314)).title("Panthapath & Mirpur Road"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.767700, 90.423000)).title("Rampura Bridge - DIT road <-> Hatirjheel"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.749859, 90.393158)).title("SAARC Fountain (Sonargaon, Bashundhara City Shopping Complex)"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.741595, 90.411856)).title("Shantinagar Mor"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.768240, 90.382861)).title("Zia Udyan"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(23.738348, 90.372999)).title("Zigatala"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(23.794847, 90.414213), 15));
 
     }
 
