d34d0f99a7a1123fe752d58017a8b987926274a6
Paul S <contact+github@winterei.se>
2017-03-04 19:50:45 +0900

    Add new points to the datastore.

--- app/src/main/java/se/winterei/rtraffic/libs/generic/PointDataStore.java
+++ app/src/main/java/se/winterei/rtraffic/libs/generic/PointDataStore.java
@@ -42,6 +42,37 @@ public class PointDataStore
         data.add(new Point(23.768240, 90.382861, "Zia Udyan", null));
         data.add(new Point(23.738348, 90.372999, "Zigatala", null));
         data.add(new Point(23.794847, 90.414213, "Gulshan Circle 2", null));
+        data.add(new Point(23.797715, 90.423513, "Notun Bazar", null));
+        data.add(new Point(23.809595, 90.421341, "Nadda", null));
+        data.add(new Point(23.811952, 90.421267, "Bashundhara Gate", null));
+        data.add(new Point(23.818811, 90.414887, "Shewra", null));
+        data.add(new Point(23.796625, 90.407494, "Banani Rd 27", null));
+        data.add(new Point(23.799114, 90.401882, "Banani Graveyard Road", null));
+        data.add(new Point(23.793589, 90.408569, "Banani Rd 27 - Kemal Ataturk", null));
+        data.add(new Point(23.791370, 90.400473, "Banani 11 - Kakoli", null));
+        data.add(new Point(23.790369, 90.407899, "Banani 11", null));
+        data.add(new Point(23.789921, 90.411179, "Banani 11", null));
+        data.add(new Point(23.780717, 90.425624, "Link Road - Pragati Sharani", null));
+        data.add(new Point(23.771430, 90.425226, "Merul Badda", null));
+        data.add(new Point(23.768240, 90.423319, "Rampura Bridge", null));
+        data.add(new Point(23.765491, 90.421816, "Rampura", null));
+        data.add(new Point(23.754172, 90.415417, "Khilgaon Abul Hotel", null));
+        data.add(new Point(23.790051, 90.416157, "Azad Masjid", null));
+        data.add(new Point(23.774244, 90.416132, "Gulshan Shooting Complex", null));
+        data.add(new Point(23.780535, 90.405433, "WireLess Mor", null));
+        data.add(new Point(23.787986, 90.399832, "Chariman Bari", null));
+        data.add(new Point(23.790517, 90.400307, "Shainik Club Bus Stop", null));
+        data.add(new Point(23.750426, 90.390355, "Bashundhara City Complex", null));
+        data.add(new Point(23.751050, 90.387090, "Panthapath - Green Road ", null));
+        data.add(new Point(23.741434, 90.396089, "Shahbagh / Mintu Road Intersection", null));
+        data.add(new Point(23.822576, 90.419733, "Nikunja", null));
+        data.add(new Point(23.756918, 90.398946, "Satrastar Mor", null));
+        data.add(new Point(23.770088, 90.401091, "Nabiscor Mor", null));
+        data.add(new Point(23.763684, 90.400059, "Bijoy Sarani - Tejgaon Link Rd", null));
+        data.add(new Point(23.744175, 90.405121, "Minto Rd", null));
+        data.add(new Point(23.745162, 90.404196, "Old Elephant Road", null));
+        data.add(new Point(23.753424, 90.400733, "Sonargaon Road (Entrance to Karwan Bazar)", null));
+
     }
 
     public List<Point> getPoints ()
