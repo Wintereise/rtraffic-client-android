e9bbff4337287e000a9c8837996063bb9b6cd4b4
Paul S <contact+github@winterei.se>
2017-03-17 13:44:28 +0900

    Fix gradle build errors due to new updates.

---
+++ app/src/main/java/se/winterei/rtraffic/activities/IntroSlider.java
@@ -0,0 +1,15 @@
+package se.winterei.rtraffic.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import se.winterei.rtraffic.R;
+
+public class IntroSlider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_slider);
+    }
+}
